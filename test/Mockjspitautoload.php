7faeed849b862f532d0382b066b811c3d1a4c2dc
jspit <pjunk@gmx.de>
2018-02-19 14:31:30 +0100

    Add files via upload

--- test/Mockjspitautoload.php
+++ test/Mockjspitautoload.php
@@ -1,1 +1,17 @@
+<?php
 
+class Mockjspitautoload extends autoload
+{
+    protected $files = array();
+
+    public function setFiles(array $files)
+    {
+        $this->files = $files;
+        return true;
+    }
+
+    protected function requireFile($file)
+    {   
+        return in_array($file, $this->files);
+    }
+}
